e51cb64a79b6a769ce91f1b3e8a91cb38fc97e88
Gonciaper <48271396+Gonciaper@users.noreply.github.com>
2019-11-25 20:30:49 +0800

    Solved some bugs

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {inputHei,inputWid,comCardHei,comCardWid,direct} from './configs/config';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App inputWid={inputWid} inputHei={inputHei} comCardWid={comCardHei} comCardWid={comCardWid} direct={direct}/>, document.getElementById('root'));
 
